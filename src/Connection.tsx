58c14fddf3997d652580a2fba7e688c048e1437b
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-04-01 11:48:55 +0300

    fix Connection component spacing

--- src/Connection.tsx
+++ src/Connection.tsx
@@ -3,7 +3,7 @@ import { Show } from 'solid-js'
  
 export default function Connection (props) {
   return <Show when={alerts.ready() !== WebSocket.OPEN} fallback={props.children}>
-    <div class="mb-4 flex items-center bg-white/45 font-light backdrop-filter backdrop-blur py-2 px-4 rounded-full shadow-xl">
+    <div class="ml-4 mb-4 flex items-center bg-white/45 font-light backdrop-filter backdrop-blur py-2 px-4 rounded-full shadow-xl">
       { alerts.ready() === WebSocket.CLOSED
         ? <>
           <svg class="w-6 h-6 mr-2 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.111 16.404a5.5 5.5 0 017.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.141 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0"></path></svg>
