d208386f0e89a891728945b2cffbe9da24a29d98
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-04-12 15:32:14 +0300

    fuse.js search tuning

--- src/worker.ts
+++ src/worker.ts
@@ -77,15 +77,15 @@ function geoDecode(place) {
   }
   let result = []
   if (~place.indexOf('область')) {
-    result = fuses[Unit.REGION].search(`^${place}`, ONE)
+    result = fuses[Unit.REGION].search(`="${place}"`, ONE)
   }
   if (~place.indexOf('район')) {
-    result = fuses[Unit.PROVINCE].search(`^${place}`, ONE)
+    result = fuses[Unit.PROVINCE].search(`="${place}"`, ONE)
   }
   if (~place.indexOf('громада')) {
-    result = fuses[Unit.DISTRICT].search(`^${place.replace('територіальна', 'міська')}`, ONE)
+    result = fuses[Unit.DISTRICT].search(`="${place}"`, ONE)
     if (!result.length) {
-      result = fuses[Unit.DISTRICT].search(`^${place}`, ONE)
+      result = fuses[Unit.DISTRICT].search(`^${place.replace('територіальна', 'міська')}`, ONE)
     }
   }
   if (place.indexOf('м') == 0) {
