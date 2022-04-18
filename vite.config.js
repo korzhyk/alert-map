cb0b9a5195aab2a0956100d00a4b9690dff886ac
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-04-18 20:17:59 +0300

    workbox for maptiler

--- vite.config.js
+++ vite.config.js
@@ -5,40 +5,78 @@ import SolidJS from 'vite-plugin-solid'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [SolidJS(), WindiCSS(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'ukrainian_geodata/hromady.geojson', 'ukrainian_geodata/rayony.geojson', 'ukrainian_geodata/regiony.geojson', 'pig-dog.geojson', 'audio.mp3'],
-    manifest: {
-      short_name: 'Повітряна тривога',
-      name: 'Мапа повітряної тривоги',
-      display: 'standalone',
-      start_url: '/',
-      background_color: '#1351af',
-      theme_color: '#1351af',
-      description: 'Інтерактивна мапа повітряної тривоги',
-      icons: [{
-        src: 'favicon.png',
-        sizes: '32x32',
-        type: 'image/png'
+  plugins: [
+    SolidJS(),
+    WindiCSS(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'apple-touch-icon.png',
+        'audio.mp3',
+        'favicon.ico',
+        'pig-dog.geojson',
+        'robots.txt',
+        'ukrainian_geodata/hromady.geojson',
+        'ukrainian_geodata/rayony.geojson',
+        'ukrainian_geodata/regiony.geojson'
+      ],
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.maptiler\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'maptiler-resources',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 // <== 1 day
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          },
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /\/preview\.png/,
+            method: 'GET'
+          }
+        ]
       },
-      {
-        src: 'android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      },
-      {
-        src: 'android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable'
-      }]
-    }
-  })],
+      manifest: {
+        short_name: 'Повітряна тривога',
+        name: 'Мапа повітряної тривоги',
+        display: 'standalone',
+        start_url: '/',
+        background_color: '#1351af',
+        theme_color: '#1351af',
+        description: 'Інтерактивна мапа повітряної тривоги',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     hmr: {
       timeout: 3000
