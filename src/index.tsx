d88f80531d9dcf7593c4e47dc782a3718a1fc79b
Andrii Korzh <Andrii.Korzh@gmail.com>
2023-08-19 16:36:47 +0300

    add attribution to capture map

--- src/index.tsx
+++ src/index.tsx
@@ -23,13 +23,44 @@ registerSW({
   }
 })
 
-document.querySelectorAll('#capture-map').forEach((button) => {
+document.querySelectorAll('[href="#capture-map"]').forEach((button) => {
   button.addEventListener('click', captureMap)
 })
 
-function captureMap() {
+function loadImage (src) {
+  if (!src) return Promise.reject(src)
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.src = src
+    img.onload = () => resolve(img)
+  })
+}
+
+async function captureMap(event) {
+  event.preventDefault()
+  
   const canvas = document.querySelector('[aria-label="Map"], canvas') as HTMLCanvasElement
-  canvas?.toBlob((blob) => {
+  let { width, height } = canvas
+
+  const [map, attribution] = await Promise.all([
+    loadImage(canvas.toDataURL()),
+    loadImage('/attribution.png')
+  ])
+
+  const blank = Object.assign(document.createElement("canvas"), { width, height })
+  const context = blank.getContext('2d')
+
+  width = attribution.width / 2
+  height = attribution.height / 2
+
+  context.drawImage(map, 0, 0)
+  const [x, y] = [
+    blank.width - width - 16,
+    blank.height - height - 16
+  ]
+  context.drawImage(attribution, x, y, width, height)
+
+  blank?.toBlob((blob) => {
     if (blob === null) return
     navigator.clipboard.write([
       new ClipboardItem({ [blob.type]: blob })
